06d192197cdaef4cff4245110b537263cb572044
davidB123KING <dbohak5@gmail.com>
2025-05-09 10:59:03 +0200

    začetek index.php
    ura zevnika

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="sl">
+    <head>
+        <title>index-stran</title>
+        <link rel="stylesheet" href="index.css">
+
+</head>
+<body>
+    <div id="zunanji">
+        <div id="meni">
+            <a href="index.php">Domov</a>
+            <a href="meni.php">Meni</a>
+            <a href="kontakt.php">Kontakt</a>
+</div>
+        <div id="notranji">
+            <h1>DOSTAVA HRANE - na dom enostavno in hitro</h1>
+            <p>naroči zdaj iz najljubših restavracij</p>
+</div>
+</div>
+</body>
+    </html>
